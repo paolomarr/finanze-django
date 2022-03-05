4bc278f1a90d8a7818c1b909fa4fc0c5d34971b6
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2022-03-05 23:00:54 +0100

    secret key for production

--- app/finanze/finanze/settings/production.py
+++ app/finanze/finanze/settings/production.py
@@ -6,6 +6,7 @@ DEBUG = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 ALLOWED_HOSTS = ['.herokuapp.com', '.localhost', '127.0.0.1']
 
+SECRET_KEY = os.environ['SECRET_KEY']
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
