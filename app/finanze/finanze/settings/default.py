06eec798f99beceeea725c68c8eb9fdbcbd315ce
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2022-05-29 09:45:26 +0200

    Trying and fixing template resolution issues

--- app/finanze/finanze/settings/default.py
+++ app/finanze/finanze/settings/default.py
@@ -56,7 +56,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            'templates'
+            'templates',
+            os.path.join(BASE_DIR, "templates"),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
