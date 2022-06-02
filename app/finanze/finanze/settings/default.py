8239296f1729874ed793f500ccb7dd323962dec7
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2022-06-02 13:00:17 +0200

    Trying to fix static assets issues

--- app/finanze/finanze/settings/default.py
+++ app/finanze/finanze/settings/default.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR,"static")
 ]
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
