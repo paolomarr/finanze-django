db149f5d06b3ac1ab00ac12bf92330745a4f11c2
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2023-01-08 20:10:50 +0100

    #? dealing with translations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,8 @@ if [[ "$DJANGO_SETTINGS_MODULE" =~ production$ ]]; then
     python app/finanze/manage.py collectstatic --ignore=*.scss
     STATIC_OPT="--nostatic"
 fi
-python app/finanze/manage.py makemessages -a
-python app/finanze/manage.py compilemessages
+if [[ "$DJANGO_SETTINGS_MODULE" =~ dev$ ]]; then
+    python app/finanze/manage.py makemessages -a
+    python app/finanze/manage.py compilemessages
+fi
 python app/finanze/manage.py runserver ${STATIC_OPT} 0.0.0.0:8000
