ccefa1411d9178f7a66caea53fa0094a543f4b85
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2023-01-27 15:43:40 +0100

    #17 introducing charts

--- app/finanze/movimenti/static/movimenti/summary.js
+++ app/finanze/movimenti/static/movimenti/summary.js
@@ -3,32 +3,75 @@ var _myDateFormat = function(date){
 	month = String(date.getMonth() + 1).padStart(2, '0');
 	day = String(date.getDate()).padStart(2, '0');
 	return `${year}-${month}-${day}`
-}
+};
 
-var fetchSummaryAjax = function(dateFrom, dateTo) {
+var _fillChart = function(chartId, data) {
+	var chart = Chart.getChart(chartId);
+	if (chart !== undefined) { // defined already
+		chart.destroy();
+	}
+	var ctx = document.getElementById(chartId);
+	new Chart(ctx, {
+		type: 'pie',
+		cutout: 0,
+		data: data,
+		options: {}
+	});
+}
+var fetchChartData = function (dateFrom, dateTo) {
+	var success = function (xhr) {
+		var data = JSON.parse(xhr.responseText);
+		var outlabels = [];
+		var inlabels = [];
+		var outvalues = [];
+		var invalues = [];
+		for(var i=0; i<data.results.length; i++){
+			item = data.results[i];
+			var inval = parseFloat(item.ins.amount);
+			var outval = parseFloat(item.outs.amount);
+			if(inval > 0.0){
+				inlabels.push(item.cat.cat);
+				invalues.push(inval);
+			}
+			if (outval > 0.0){
+				outlabels.push(item.cat.cat)
+				outvalues.push(outval);
+			}
+		}
+		_fillChart('expensesChart', { labels: outlabels, datasets: [{data: outvalues, label: "Expenses"}] });
+		_fillChart('incomesChart', { labels: inlabels, datasets: [{data: invalues, label: "Incomes"}] });
+	};
+	_fetch(dateFrom, dateTo, "json", success);
+};
+var fetchSummaryAjax = function (dateFrom, dateTo) {
+	var success = function(xhr) {
+		document.getElementById('summaryTableContainer').innerHTML = xhr.responseText;
+	};
+	_fetch(dateFrom, dateTo, "fetch", success);
+}
+var _fetch = function(dateFrom, dateTo, mode, success) {
 	// string dates
 	dfString = _myDateFormat(dateFrom);
 	dtString = _myDateFormat(dateTo);
 	var XHR = new XMLHttpRequest();
 	XHR.addEventListener('load', function () {
-		// renderSummaryTable(JSON.parse(XHR.responseText));
-		document.getElementById('summaryTableContainer').innerHTML = XHR.responseText;
+		success(XHR);
 	});
 	XHR.addEventListener('error', function() {
 		console.log(XHR.statusText);
 	});
-	var url = "/movimenti/summary/fetch";
+	var url = `/movimenti/summary/${mode}`;
 	var params = { dateFrom: dateFrom.toISOString(), dateTo: dateTo.toISOString() };
 	var search = new URLSearchParams(params);
 	XHR.open('GET', url + "?" + search.toString());
 	XHR.send();
 }
-var buildURLFromMonthYearSelects = function() {
+var buildDateRangeFromSelects = function() {
 	var monthindex = document.getElementById('select-month').value - 1; // they come 1-based
 	var year = document.getElementById('select-year').value;
 	var dateFrom = new Date(year, monthindex, 1);
 	var dateTo = new Date(year, monthindex + 1, 1);
-	fetchSummaryAjax(dateFrom, dateTo);
+	return [dateFrom, dateTo];
 }
 var fetchSummaryFromDateRangeFields = function () {
 	from = document.getElementById('dateFrom').value;
@@ -40,17 +83,25 @@ var fetchSummaryFromDateRangeFields = function () {
 }
 window.addEventListener("load", function(){
 	document.getElementById('select-month').addEventListener("change", () => {
-		buildURLFromMonthYearSelects();
+		var range = buildDateRangeFromSelects();
+		fetchSummaryAjax(range[0], range[1]);
+		fetchChartData(range[0], range[1]);
 	});
 	document.getElementById('select-year').addEventListener("change", () => {
-		buildURLFromMonthYearSelects();
+		var range = buildDateRangeFromSelects();
+		fetchSummaryAjax(range[0], range[1]);
+		fetchChartData(range[0], range[1]);
 	});
-	this.document.getElementById("dateFrom").addEventListener("change", () => {
+	document.getElementById("dateFrom").addEventListener("change", () => {
 		fetchSummaryFromDateRangeFields();
+		fetchChartData(range[0], range[1]);
 	});
-	this.document.getElementById("dateTo").addEventListener("change", () => {
+	document.getElementById("dateTo").addEventListener("change", () => {
 		fetchSummaryFromDateRangeFields();
+		fetchChartData(range[0], range[1]);
 	});
 
-	buildURLFromMonthYearSelects();
+	var range = buildDateRangeFromSelects();
+	fetchSummaryAjax(range[0], range[1]);
+	fetchChartData(range[0], range[1]);
 });
