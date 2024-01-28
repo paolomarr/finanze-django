f1a4229e71fe1738ce1e7be9492a3fdb1ca84b22
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2024-01-28 15:07:45 +0100

    #?, narrower login form

--- movimenti-fe/src/components/LoginForm.jsx
+++ movimenti-fe/src/components/LoginForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, FormGroup, Input, Label, Button, Container } from "reactstrap";
+import { Form, FormGroup, Input, Label, Button, Container, Row, Col } from "reactstrap";
 import { Navigate } from "react-router-dom";
 import { useState } from "react";
 import authenticate from "../queries/authenticate";
@@ -27,31 +27,35 @@ const LoginForm = () => {
     <Container>
       {error && <p>{error.message}</p>}
       {user && <Navigate to="/" replace={true} />}
-      <Form onSubmit={(event) => handleSubmit(event)}>
-        <FormGroup>
-          <Label for="username" hidden>
-            Username
-          </Label>
-          <Input
-            id="username"
-            name="username"
-            placeholder="Username"
-            type="username"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="password" hidden>
-            Password
-          </Label>
-          <Input
-            id="password"
-            name="password"
-            placeholder="Password"
-            type="password"
-          />
-        </FormGroup>
-        <Button>Submit</Button>
-      </Form>
+      <Row className="justify-content-center">
+        <Col className="col-md-6 col-lg-4">
+          <Form onSubmit={(event) => handleSubmit(event)}>
+            <FormGroup>
+              <Label for="username" hidden>
+                Username
+              </Label>
+              <Input
+                id="username"
+                name="username"
+                placeholder="Username"
+                type="username"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password" hidden>
+                Password
+              </Label>
+              <Input
+                id="password"
+                name="password"
+                placeholder="Password"
+                type="password"
+              />
+            </FormGroup>
+            <Button>Submit</Button>
+          </Form>
+        </Col>
+      </Row>
     </Container>
   );
 };
