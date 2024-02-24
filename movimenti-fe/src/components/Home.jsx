f438834f13e533db55438b8a623888b7e6e304b8
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2024-02-24 17:59:27 +0100

    #?, double range slider to set time range for movements history graph

--- movimenti-fe/src/components/Home.jsx
+++ movimenti-fe/src/components/Home.jsx
@@ -10,22 +10,27 @@ import { t } from "@lingui/macro"
 
 const Home = () => {
     const {i18n} = useLingui()
-    const printTemporalDate = ((temporalZonedDateTime) => {
-      return i18n.date(temporalZonedDateTime.toInstant().epochMilliseconds);
+    const printDate = ((date) => {
+      return i18n.date(date);
       // const options = { year: 'numeric', month: 'short', day: 'numeric' };
       // return temporalZonedDateTime.toLocaleString('it-IT', options);
     });
 
-    const today = Temporal.Now.zonedDateTimeISO();
+    const today = new Date();
     const threeMonthsAgo = Temporal.Now.zonedDateTimeISO().subtract({months: 1});
-    const [dateRange] = useState([threeMonthsAgo, today]);
+    const [dateRange, setDateRange] = useState([new Date(threeMonthsAgo.epochMilliseconds), today]);
 
     const results = useQuery({
-      queryKey: ["movements"],
+      queryKey: ["movements", "all"],
       queryFn: fetchMovements,
       retry: (failureCount, error) => {
         if(error.message === "forbidden") return false;
         else return 3;
+      },
+      onSuccess: (data) => {
+        const from = new Date(data.minDate);
+        const to = new Date(data.maxDate);
+        setDateRange([from, to]);
       }});
     if (results.isLoading) {
       return (
@@ -48,7 +53,7 @@ const Home = () => {
     }
     return (
         <>
-            <h3 className="text-center">{t({id: "date.from", message: "From"})} {printTemporalDate(dateRange[0])} {t({id: "date.to", message: "to"})} {printTemporalDate(dateRange[1])}</h3>
+            <h3 className="text-center">{t({id: "date.from", message: "From"})} {printDate(dateRange[0])} {t({id: "date.to", message: "to"})} {printDate(dateRange[1])}</h3>
             <MovementsHistory data={results.data}/>
             <MovementsList movements={results.data.movements} refresh={results.refetch}/>
         </>
--- movimenti-fe/src/components/MovementsHistory.jsx
+++ movimenti-fe/src/components/MovementsHistory.jsx
@@ -1,31 +1,63 @@
 import { useState } from 'react';
 import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
+import MultiRangeSlider from "multi-range-slider-react";
+import { sub } from 'date-fns' 
 
 
 const MovementsHistory = (props) => {
     const rawdata = props.data;
-    const [timeRange] = useState([(new Date(rawdata.minDate)).getTime(), (new Date(rawdata.maxDate)).getTime()]);
+    const initialStart = sub(new Date(), {days: 90});
+    const initialEnd = new Date();
+    const initialMin = new Date(rawdata.minDate);
+    const initialMax = new Date(rawdata.maxDate);
+
+    const [timeRange] = useState([initialMin, initialMax]);
+    const [step] = useState((initialMax - initialMin)/100);
+    const [selectedInterval] = useState([initialStart, initialEnd]);
+
     const baselineData = rawdata.baseline?? null;
-    let rawmovements = rawdata.movements.toReversed();
-    
+
     let data;
     if(baselineData){
-        // const baselineDate = new Date(baselineData[0]);
         const baselineVal = baselineData[1];
         let cumulative = baselineVal;
-        data = rawmovements.map((movement) => {
+        data = [];
+        for(let i=rawdata.movements.length-1; i>0; i--){ // reverse
+            const movement = rawdata.movements[i];
             cumulative += movement.abs_amount * movement.category.direction;
-            return {"date": (new Date(movement.date)).getTime(), "cumulative": cumulative};
-        });
+            data.push({"date": (new Date(movement.date)).getTime(), "cumulative": cumulative})
+        }
     }
+    const slicedData = (changeResult) => {
+      const minPercent = (changeResult.minValue - changeResult.min) / (changeResult.max - changeResult.min);
+      const maxPercent = (changeResult.maxValue - changeResult.min) / (changeResult.max - changeResult.min);
+      const startIdx = parseInt(minPercent * data.length);
+      const endIdx = parseInt(maxPercent * data.length);
+      return data.slice(startIdx, endIdx);
+    }
+    const initialResult = {
+      min: timeRange[0], max: timeRange[1], minValue: selectedInterval[0], maxValue: selectedInterval[1]
+    };
+    const initialShownData = slicedData(initialResult);
+    const [shownData, setShownData] = useState(initialShownData);
+
+    const handleInput = (changeResult) => {
+    //   setSelectedTimeInterval([new Date(changeResult.minValue), new Date(changeResult.maxValue)]);
+      setShownData(slicedData(changeResult));
+      // console.log(changeResult);
+    };
+    
+    
     return (
+      <>
         <ResponsiveContainer width="100%" height={400}>
-            <LineChart data={data}>
+            <LineChart data={shownData}>
                 <Line type="stepAfter" dataKey="cumulative" stroke="#8884d8" dot={false} />
                 <XAxis 
                     type='number' 
                     dataKey="date"  
-                    domain={timeRange} 
+                    // domain={selectedInterval} 
+                    domain={["auto", "auto"]} 
                     tickFormatter={tick => (new Date(tick)).toLocaleDateString()}
                     tickCount="10" />
                 <YAxis domain={["auto", "auto"]}/> 
@@ -33,8 +65,21 @@ const MovementsHistory = (props) => {
                     active={true} 
                     formatter={(value) => `${parseFloat(value).toFixed(2)} €`}
                     labelFormatter={(timestamp) => (new Date(timestamp)).toLocaleDateString()} />
+                {/* <Brush dataKey="date" /> */}
             </LineChart>
         </ResponsiveContainer>
+        <MultiRangeSlider 
+            min={timeRange[0]}
+            max={timeRange[1]}
+            step={step}
+            minValue={selectedInterval[0]}
+            maxValue={selectedInterval[1]}
+            ruler={false}
+            onInput={(e)=>{
+              handleInput(e);
+            }}
+        />
+      </>
     )
 }
 
