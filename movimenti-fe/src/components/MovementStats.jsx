584e5aea1d1ea5588adf02ebd780b24c310ef170
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2024-04-02 18:14:50 +0200

    WIP: two separate barcharts for expenses and earnings
    responsiveness: charts side2side when large screen, stacked when smaller screen.

--- movimenti-fe/src/components/MovementStats.jsx
+++ movimenti-fe/src/components/MovementStats.jsx
@@ -1,10 +1,12 @@
-import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip, Cell, LabelList } from 'recharts';
+import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip, LabelList } from 'recharts';
+import { Row, Col } from 'reactstrap';
+import { useMediaQuery } from 'react-responsive'
 
 const CustomTooltip = ({ active, payload, label }) => {
   if (active && payload && payload.length) {
-    let valString = `${parseFloat(payload[0].value).toFixed(2)}`;
+    let valString = `${parseFloat(payload[0].payload.sum).toFixed(2)}`;
     if(payload[0].payload.percent){
-      valString += ` (${parseFloat(payload[0].payload.percent).toFixed(2)*100}%)`;
+      valString += ` (${parseFloat(payload[0].value).toFixed(1)}%)`;
     }
     return (
       <div className="custom-tooltip">
@@ -19,16 +21,20 @@ const CustomTooltip = ({ active, payload, label }) => {
 
 const MovementsStats = ({data}) => {
     const percent_cutin = 0.05;
-    let localData = [];
+    let earningData = {
+      earnings: [],
+    };
+    let expensesData = {
+      expenses: [],
+    };
     if(data) {
-      localData.push({category: "TOT_IN", sum: data.incomes, direction: 1});
-      localData.push({category: "TOT_OUT", sum: data.outcomes, direction: -1});
-      let incomes = [];
-      let outcomes = [];
+      earningData.totals = data.incomes;
+      expensesData.totals = data.outcomes;
       let outliers = {
         incomes: {category: "Other in.", sum: 0, percent: 0, direction: 1},
         outcomes: {category: "Other out.", sum: 0, percent: 0, direction: -1},
       }
+
       for (const catName in data.categories) {
         const cat = data.categories[catName];
         let percent = 0.;
@@ -36,7 +42,7 @@ const MovementsStats = ({data}) => {
           percent = cat.sum/data.incomes;
           const newitem = {...cat, "percent": percent};
           if(percent>=percent_cutin){
-            incomes.push(newitem)
+            earningData.earnings.push(newitem);
           }else{
             outliers.incomes.sum += newitem.sum;
             outliers.incomes.percent += newitem.sum;
@@ -45,55 +51,77 @@ const MovementsStats = ({data}) => {
           percent = cat.sum/data.outcomes;
           const newitem = {...cat, "percent": percent};
           if(percent>=percent_cutin){
-            outcomes.push(newitem)
+            expensesData.expenses.push(newitem)
           }else{
             outliers.outcomes.sum += newitem.sum;
             outliers.outcomes.percent += newitem.percent;
           }
         }
       }
-      incomes.sort((a,b)=> b.sum - a.sum); // reverse sorted
-      localData.push(...incomes);
+      earningData.earnings.sort((a,b)=> b.sum - a.sum); // reverse sorted
       if(outliers.incomes.sum > 0){
-        localData.push(outliers.incomes)
+        earningData.earnings.push(outliers.incomes)
       }
-      outcomes.sort((a,b)=> b.sum - a.sum); // reverse sorted
-      localData.push(...outcomes);
+      expensesData.expenses.sort((a,b)=> b.sum - a.sum); // reverse sorted
       if(outliers.outcomes.sum > 0){
-        localData.push(outliers.outcomes);
+        expensesData.expenses.push(outliers.outcomes);
       }
     }
     const colors = {
       "1": "#66c2a5",
       "-1": "#fc8d62",
     }
+    const isLargeScreen = useMediaQuery({minWidth: 960 });
     const labelContentRenderer = (props) => {
       const { x, y, width, height, fill, value } = props;
-      return (
-        <text x={x + width + 10} y={y + height/2} fill={fill} textAnchor="start" dominantBaseline="middle">
-        {value.split(' ')[0]}
-      </text>
-      )
+      if(isLargeScreen){
+        return (
+        <text x={x} y={y-10} fill={fill} textAnchor="center" dominantBaseline="middle">
+          {value.split(' ')[0]}
+        </text>
+        )
+      }else{
+        return (
+          <text x={x+width+10} y={y+height/2} fill={fill} textAnchor="start" dominantBaseline="middle">
+          {value.split(' ')[0]}
+        </text>
+        )
+      }
     };
+    
     return (
-      <ResponsiveContainer width="100%" height={800}>
-          <BarChart data={localData} 
-            // height={"500px"}
-            layout='vertical' 
-            barCategoryGap={'20%'} >
-            <XAxis type='number'/>
-            <YAxis type='category' dataKey="category" tick={false} />
-            <Tooltip content={<CustomTooltip />}/>
-            <Bar dataKey={"sum"}>
-              {
-                localData.map((entry, index) => (
-                  <Cell key={`cell-${index}`} fill={colors[entry.direction]} />
-                ))
-              }
-              <LabelList dataKey="category" position="top" angle={0} content={labelContentRenderer} />
-            </Bar>
-          </BarChart>
-      </ResponsiveContainer>
+      <Row>
+        <Col xs="12" lg="4">
+          <ResponsiveContainer height={400} width={"100%"} >
+            <BarChart data={earningData.earnings}
+              // width={150} height={40}
+              // layout= {isLargeScreen ? 'horizontal':'vertical'}
+              barCategoryGap={'10%'} >
+              <YAxis type='number' label={{value:"%", position:"top"}} domain={[0, 100]}/>
+              <XAxis type='category' dataKey="category" tick={false} />
+              <Tooltip content={<CustomTooltip />}/>
+              <Bar dataKey={(item)=> 100*item.percent} fill={colors["1"]}>
+                <LabelList dataKey="category" position="end" angle={0} content={labelContentRenderer} />
+              </Bar>
+            </BarChart>
+          </ResponsiveContainer>
+        </Col>
+        <Col xs="12" lg="8">
+          <ResponsiveContainer height={400} width={"100%"}>
+            <BarChart data={expensesData.expenses}
+              // width={150} height={40}
+              // layout= {isLargeScreen ? 'horizontal':'vertical'}
+              barCategoryGap={'10%'} >
+              <YAxis type='number' label={{value:"%", position:"top"}} domain={[0, 100]}/>
+              <XAxis type='category' dataKey="category" tick={false} />
+              <Tooltip content={<CustomTooltip />}/>
+              <Bar dataKey={(item)=> 100*item.percent} fill={colors["-1"]}>
+                <LabelList dataKey="category" position="end" angle={0} content={labelContentRenderer} />
+              </Bar>
+            </BarChart>
+          </ResponsiveContainer>
+        </Col>
+      </Row>
     )
 };
 
