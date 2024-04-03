51816d106d4bbc3c0bee64c8c13bf5c0753056eb
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2024-04-03 10:11:16 +0200

    #29 better responsive bar charts

--- movimenti-fe/src/components/MovementStats.jsx
+++ movimenti-fe/src/components/MovementStats.jsx
@@ -1,7 +1,7 @@
 import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip, LabelList, Cell } from 'recharts';
 import { Row, Col } from 'reactstrap';
 import { useMediaQuery } from 'react-responsive'
-import { t, Trans } from "@lingui/macro";
+import { t } from "@lingui/macro";
 
 
 const CustomTooltip = ({ active, payload, label }) => {
@@ -74,19 +74,77 @@ const MovementsStats = ({data}) => {
       "-1": "#fc8d62",
     }
     const isLargeScreen = useMediaQuery({minWidth: 960 });
-    const labelContentRenderer = (props) => {
-      const { x, y, width, height, fill, value } = props;
+    // const labelContentRenderer = (props) => {
+    //   const { x, y, width, height, fill, value } = props;
+    //   if(isLargeScreen){
+    //     return (
+    //     <text x={x} y={y-10} fill={fill} textAnchor="center" dominantBaseline="middle">
+    //       {value.split(' ')[0]}
+    //     </text>
+    //     )
+    //   }else{
+    //     return (
+    //       <text x={x+width+10} y={y+height/2} fill={fill} textAnchor="start" dominantBaseline="middle">
+    //       {value.split(' ')[0]}
+    //     </text>
+    //     )
+    //   }
+    // };
+    const ResponsiveBarChart = ({data, dataKey, title, label}) => {
+      const defaultLabel = {value:"%", position:"insideTopLeft", offset: 10};
+      if(label) {
+        label = {...defaultLabel, ...label};
+      }else{
+        label = defaultLabel;
+      }
       if(isLargeScreen){
         return (
-        <text x={x} y={y-10} fill={fill} textAnchor="center" dominantBaseline="middle">
-          {value.split(' ')[0]}
-        </text>
+          <>
+            <ResponsiveContainer height={400} width={"90%"} >
+              <BarChart data={data}
+                margin={{top:20, left:0, right:0, bottom:0}}
+                barCategoryGap={'10%'} >
+                <YAxis type='number' label={label} />
+                <XAxis type='category' dataKey="category" tick={false} />
+                <Tooltip content={<CustomTooltip />}/>
+                <Bar dataKey={dataKey}>
+                  {
+                    data.map((item, index) => (
+                      <Cell key={`bar_${index}`} fill={item.direction === 1 ? colors["1"]: colors["-1"]} />
+                    ))
+                  }
+                  <LabelList dataKey="category" position="center" textAnchor='center' width={400} fill='#666' angle={-90} />
+                </Bar>
+              </BarChart>
+            </ResponsiveContainer>
+            <div className='bar-chart-title'>{title}</div>
+          </>
         )
       }else{
+        const barSize = 30;
+        const chartHeight = barSize * (data.length + 1);
         return (
-          <text x={x+width+10} y={y+height/2} fill={fill} textAnchor="start" dominantBaseline="middle">
-          {value.split(' ')[0]}
-        </text>
+          <>
+            <div className='bar-chart-title'>{title}</div>
+            <ResponsiveContainer height={chartHeight} width={"100%"} >
+              <BarChart data={data}
+                layout='vertical'
+                margin={{top:0, left:0, right:0, bottom:0}}
+                barCategoryGap={'10%'} >
+                <XAxis type='number' label={label} />
+                <YAxis type='category' dataKey="category" tick={false} hide={true} />
+                <Tooltip content={<CustomTooltip />}/>
+                <Bar dataKey={dataKey} barSize={barSize} >
+                  {
+                    data.map((item, index) => (
+                      <Cell key={`bar_${index}`} fill={item.direction === 1 ? colors["1"]: colors["-1"]} />
+                    ))
+                  }
+                  <LabelList dataKey="category" position="insideLeft" width={400} angle={0} fill='#666' />
+                </Bar>
+              </BarChart>
+            </ResponsiveContainer>
+          </>
         )
       }
     };
@@ -97,54 +155,13 @@ const MovementsStats = ({data}) => {
     return (
       <Row className='mb-2'>
         <Col xs="12" lg={earningsChartWidth}>
-          <ResponsiveContainer height={400} width={"90%"} >
-            <BarChart data={earningData.earnings}
-              margin={{top:20, left:0, right:0, bottom:0}}
-              barCategoryGap={'10%'} >
-              <YAxis type='number' label={{value:"%", position:"insideTopLeft", offset: 10}} />
-              <XAxis type='category' dataKey="category" tick={false} />
-              <Tooltip content={<CustomTooltip />}/>
-              <Bar dataKey={(item)=> 100*item.percent} fill={colors["1"]}>
-                <LabelList dataKey="category" position="end" angle={0} content={labelContentRenderer} />
-              </Bar>
-            </BarChart>
-          </ResponsiveContainer>
-          <div className='bar-chart-title'><Trans>Earnings</Trans></div>
+          <ResponsiveBarChart title={t`Earnings`} data={earningData.earnings}  dataKey={(item)=> 100*item.percent}/>
         </Col>
         <Col xs="12" lg={expensedChartWidth}>
-          <ResponsiveContainer height={400} width={"90%"}>
-            <BarChart data={expensesData.expenses}
-              margin={{top:20, left:0, right:0, bottom:0}}
-              barCategoryGap={'10%'} >
-              <YAxis type='number' label={{value:"%", position:"insideTopLeft", offset: 10}} />
-              <XAxis type='category' dataKey="category" tick={false} />
-              <Tooltip content={<CustomTooltip />}/>
-              <Bar dataKey={(item)=> 100*item.percent} fill={colors["-1"]}>
-                <LabelList dataKey="category" position="end" angle={0} content={labelContentRenderer} />
-              </Bar>
-            </BarChart>
-          </ResponsiveContainer>
-          <div className='bar-chart-title'><Trans>Expenses</Trans></div>
+          <ResponsiveBarChart title={t`Expenses`} data={expensesData.expenses} dataKey={(item)=> 100*item.percent}/>
         </Col>
         <Col xs="12" lg="3">
-          <ResponsiveContainer height={400} width={"90%"}>
-            <BarChart data={totalsData}
-              margin={{top:20, left:0, right:0, bottom:0}}
-              barCategoryGap={'10%'} >
-              <YAxis type='number' label={{value:"€", position:"insideTopLeft", offset: 10}} />
-              <XAxis type='category' dataKey="category" tick={false} />
-              <Tooltip content={<CustomTooltip />}/>
-              <Bar dataKey={"sum"}>
-                {
-                  totalsData.map((item, index) => (
-                    <Cell key={`bar_${index}`} fill={item.category === "Earnings" ? colors["1"]: colors["-1"]} />
-                  ))
-                }
-                <LabelList dataKey="category" position="end" angle={0} content={labelContentRenderer} />
-              </Bar>
-            </BarChart>
-          </ResponsiveContainer>
-          <div className='bar-chart-title'><Trans>Totals</Trans></div>
+          <ResponsiveBarChart title={t`Totals`} label={{value: "€"}} data={totalsData} dataKey={"sum"}/>
         </Col>
       </Row>
     )
