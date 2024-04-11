df403972cc81032e5041d55e4c4a88e1ab60241f
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2024-04-11 23:03:39 +0200

    #?, wip

--- movimenti-fe/src/components/MovementStats.jsx
+++ movimenti-fe/src/components/MovementStats.jsx
@@ -1,4 +1,4 @@
-import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip, LabelList, Cell } from 'recharts';
+import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip, LabelList, Cell, Rectangle } from 'recharts';
 import { Row, Col } from 'reactstrap';
 import { useMediaQuery } from 'react-responsive'
 import { t } from "@lingui/macro";
@@ -93,51 +93,38 @@ const MovementsStats = ({data, slice, categories}) => {
     }
     totalsData = [{direction: 1, category: t`Earnings`, sum: incomes}, {direction: -1, category: t`Expenses`, sum: outcomes}];
 
-
-    // if(data) {
-    //   totalsData = [{direction: 1, category: t`Earnings`, sum: data.incomes}, {direction: -1, category: t`Expenses`, sum: data.outcomes}];
-    //   let outliers = {
-    //     incomes: {category: t`Other in.` + ` (<${100*percent_cutin}%)`, sum: 0, percent: 0, direction: 1},
-    //     outcomes: {category: t`Other out.` + ` (<${100*percent_cutin}%)`, sum: 0, percent: 0, direction: -1},
-    //   }
-
-    //   for (const catName in data.categories) {
-    //     const cat = data.categories[catName];
-    //     let percent = 0.;
-    //     if(cat.direction === 1) {
-    //       percent = cat.sum/data.incomes;
-    //       const newitem = {...cat, "percent": percent};
-    //       if(percent>=percent_cutin){
-    //         earningData.earnings.push(newitem);
-    //       }else{
-    //         outliers.incomes.sum += newitem.sum;
-    //         outliers.incomes.percent += newitem.sum;
-    //       }
-    //     }else{
-    //       percent = cat.sum/data.outcomes;
-    //       const newitem = {...cat, "percent": percent};
-    //       if(percent>=percent_cutin){
-    //         expensesData.expenses.push(newitem)
-    //       }else{
-    //         outliers.outcomes.sum += newitem.sum;
-    //         outliers.outcomes.percent += newitem.percent;
-    //       }
-    //     }
-    //   }
-    //   earningData.earnings.sort((a,b)=> b.sum - a.sum); // reverse sorted
-    //   if(outliers.incomes.sum > 0){
-    //     earningData.earnings.push(outliers.incomes)
-    //   }
-    //   expensesData.expenses.sort((a,b)=> b.sum - a.sum); // reverse sorted
-    //   if(outliers.outcomes.sum > 0){
-    //     expensesData.expenses.push(outliers.outcomes);
-    //   }
-    // }
     const chart_colors = {
       "1": colors.primary,
       "-1": colors.error,
     }
     const isLargeScreen = useMediaQuery({minWidth: 960 });
+
+    const CustomBarShape = (props) => {
+      const { x, y, width, height, fill, barSize } = props;
+      // let theX=x, theY=y, theOffsetX=0, theOffsetY=10;
+      let radius = [0, 10, 10, 0]; // clockwise, starting from topleft corner
+      let theOffsetY = 10, theOffsetX=0;
+      let theWidth=width, theHeight=barSize;
+      if(isLargeScreen){
+      //   // theX=y; theY=x;
+      //   // theOffsetX=10; 
+        theOffsetY=0;
+        theWidth=barSize;
+        theHeight=height;
+        theOffsetX=(width-barSize)/2;
+        radius = [10,10,0,0];
+      }
+      // return <Rectangle x={x} y={y} width={width} height={barSize} fill={fill} radius={10}></Rectangle>
+      return <Rectangle x={x+theOffsetX} y={y+theOffsetY} width={theWidth} height={theHeight} fill={fill} radius={radius}></Rectangle>
+    };
+    const renderCustomisedLabel = (props) => {
+      const { x, y,  value, fill } = props;
+      return (
+        <text x={x} y={y+2} fill={fill} textAnchor="start" dominantBaseline="middle">
+          {value}
+        </text>
+      );
+    }
     
     const ResponsiveBarChart = ({data, dataKey, title, label}) => {
       const defaultLabel = {value:"%", position:"insideTopLeft", offset: 10};
@@ -146,6 +133,7 @@ const MovementsStats = ({data, slice, categories}) => {
       }else{
         label = defaultLabel;
       }
+      const barSize = 8;
       if(isLargeScreen){
         return (
           <>
@@ -154,15 +142,15 @@ const MovementsStats = ({data, slice, categories}) => {
                 margin={{top:20, left:0, right:0, bottom:0}}
                 barCategoryGap={'10%'} >
                 <YAxis type='number' label={label} />
-                <XAxis type='category' dataKey="category" tick={false} />
+                <XAxis type='category' dataKey="category" tick={true} tickLine={false} />
                 <Tooltip content={<CustomTooltip />}/>
-                <Bar dataKey={dataKey}>
+                <Bar dataKey={dataKey} shape={<CustomBarShape barSize={barSize}/>}>
                   {
                     data.map((item, index) => (
                       <Cell key={`bar_${index}`} fill={item.direction === 1 ? chart_colors["1"]: chart_colors["-1"]} />
                     ))
                   }
-                  <LabelList dataKey="category" position="center" textAnchor='center' width={400} fill='#666' angle={-90} />
+                  {/* <LabelList dataKey="category" position="center" textAnchor='center' width={400} fill='#666' angle={-90} /> */}
                 </Bar>
               </BarChart>
             </ResponsiveContainer>
@@ -170,26 +158,29 @@ const MovementsStats = ({data, slice, categories}) => {
           </>
         )
       }else{
-        const barSize = 30;
-        const chartHeight = barSize * (data.length + 1);
+        const chartHeight = (5*barSize) * (data.length + 1);
         return (
           <>
             <div className='bar-chart-title'>{title}</div>
             <ResponsiveContainer height={chartHeight} width={"100%"} >
               <BarChart data={data}
                 layout='vertical'
-                margin={{top:0, left:0, right:0, bottom:0}}
-                barCategoryGap={'10%'} >
+                margin={{top:10, left:0, right:0, bottom:0}}
+                barCategoryGap={"5%"} >
                 <XAxis type='number' label={label} />
                 <YAxis type='category' dataKey="category" tick={false} hide={true} />
                 <Tooltip content={<CustomTooltip />}/>
-                <Bar dataKey={dataKey} barSize={barSize} >
+                <Bar dataKey={dataKey} shape={<CustomBarShape barSize={barSize} />}>
                   {
                     data.map((item, index) => (
                       <Cell key={`bar_${index}`} fill={item.direction === 1 ? chart_colors["1"]: chart_colors["-1"]} />
                     ))
                   }
-                  <LabelList dataKey="category" position="insideLeft" width={400} angle={0} fill='#666' />
+                  <LabelList dataKey="category" 
+                    // position="insideTopLeft" offset={0} 
+                    content={renderCustomisedLabel}
+                    // width={400} angle={0} fill='#666' 
+                    />
                 </Bar>
               </BarChart>
             </ResponsiveContainer>
