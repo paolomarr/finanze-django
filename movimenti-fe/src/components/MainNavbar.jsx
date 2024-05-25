4e29c06ec204795b1f39866e009a84a19391542d
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2024-05-25 17:14:29 +0200

    #?, improving routes

--- movimenti-fe/src/components/MainNavbar.jsx
+++ movimenti-fe/src/components/MainNavbar.jsx
@@ -2,7 +2,6 @@ import { useMediaQuery } from "react-responsive";
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faLanguage, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
-import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
@@ -11,14 +10,31 @@ import { Trans } from "@lingui/macro";
 import Dropdown from 'react-bootstrap/Dropdown'
 import { languages } from "../constants";
 import { useLingui } from "@lingui/react";
+import { NavLink } from "react-router-dom";
 
-const OffcanvasNavbar = () => {
+const RightButtonGroup = () => {
     const { i18n } = useLingui();
     const setLanguage = (locale) => {
         localStorage.setItem("user_locale", locale);
         i18n.activate(locale);
     };
-    
+    return <ButtonGroup>
+        <Dropdown as={ButtonGroup}>
+            <Dropdown.Toggle variant="secondary">
+                <FontAwesomeIcon icon={faLanguage} />
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                <Dropdown.Header><Trans>Languages</Trans></Dropdown.Header>
+                { languages.map((loc)=> {return <Dropdown.Item className={loc.locale === i18n.locale ? "fw-bold" : ""} key={loc.locale} onClick={() => setLanguage(loc.locale)}>{loc.name}</Dropdown.Item>} )
+                    }
+            </Dropdown.Menu>
+        </Dropdown>
+        <Button variant="secondary" href="/logout">
+            <FontAwesomeIcon icon={faRightFromBracket} />
+        </Button>
+    </ButtonGroup>
+};
+const OffcanvasNavbar = () => {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
@@ -36,34 +52,46 @@ const OffcanvasNavbar = () => {
                 <Offcanvas.Body>
                     <Nav className="flex-column">
                         <Nav.Item>
-                            <Nav.Link href="/"><Trans>Movements overview</Trans></Nav.Link>
+                            <NavLink to="/" end
+                            className={({isActive}) => "nav-link" + (isActive ? " active" : "")}
+                            onClick={() => setShow(false)}>
+                                <Trans>Movements</Trans>
+                            </NavLink>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href="/assets"><Trans>Balance records</Trans></Nav.Link>
+                            <NavLink to="/assets" end
+                            className={({isActive}) => "nav-link" + (isActive ? " active" : "")}
+                            onClick={() => setShow(false)}>
+                                <Trans>Balance records</Trans>
+                            </NavLink>
                         </Nav.Item>
                     </Nav>
                 </Offcanvas.Body>
             </Offcanvas>
         </div>
-        <ButtonGroup>
-            <Dropdown as={ButtonGroup}>
-                <Dropdown.Toggle variant="secondary">
-                    <FontAwesomeIcon icon={faLanguage} />
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    <Dropdown.Header><Trans>Languages</Trans></Dropdown.Header>
-                    { languages.map((loc)=> {return <Dropdown.Item className={loc.locale === i18n.locale ? "fw-bold" : ""} key={loc.locale} onClick={() => setLanguage(loc.locale)}>{loc.name}</Dropdown.Item>} )
-                        }
-                </Dropdown.Menu>
-            </Dropdown>
-            <Button variant="secondary" href="/logout">
-                <FontAwesomeIcon icon={faRightFromBracket} />
-            </Button>
-        </ButtonGroup>
+        <RightButtonGroup />
     </div>
 };
 const StandardNavbar = () => {
-    return <Navbar></Navbar>
+    return <div className="m-2 d-flex">
+        <div className="me-auto">
+            <Nav variant="underline">
+                <Nav.Item>
+                    <NavLink to="/" end
+                    className={({isActive}) => "nav-link" + (isActive ? " active" : "")}>
+                        <Trans>Movements</Trans>
+                    </NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                    <NavLink to="/assets" end
+                    className={({isActive}) => "nav-link" + (isActive ? " active" : "")}>
+                        <Trans>Balance records</Trans>
+                    </NavLink>
+                </Nav.Item>
+            </Nav>
+        </div>
+        <RightButtonGroup />
+    </div>
 };
 const MainNavbar = () => {
     const isMobile = useMediaQuery({ query: '(max-width: 576px)' })
