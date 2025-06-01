0481e10f5a6b1978698aa5b3a6893a8d51c7d646
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2025-06-01 17:17:02 +0200

    improve movement history chart
    exploit new info coming from movements list API endpoint, avoiding useless computation over huge data sets

--- movimenti-fe/src/components/MovementsHistory.jsx
+++ movimenti-fe/src/components/MovementsHistory.jsx
@@ -1,84 +1,57 @@
-import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip, LabelList } from 'recharts';
 import Form from 'react-bootstrap/Form';
 import { format } from '../_lib/format_locale';
 import { useLingui } from '@lingui/react';
 import { useState } from 'react';
 import { t } from '@lingui/macro';
 
+function BalanceManager() {
+    this.groupedMovements = {};
+    this.addMovement = function(movement){
+        const mdate = new Date(movement.date);
+        if(mdate.getTime() in this.groupedMovements){
+            this.groupedMovements[mdate.getTime()].movements.push(movement);
+            this.groupedMovements[mdate.getTime()].groupSum += movement.abs_amount;
+        }else{
+            this.groupedMovements[mdate.getTime()] = {
+                "movements": [movement],
+                "groupSum": movement.abs_amount,
+            }
+        }
+    }
+    this.timeSeries = function(){
+        var series = [];
+        for (const timestamp in this.groupedMovements) {
+            series.push({"date": parseInt(timestamp), "balance": this.groupedMovements[timestamp].groupSum});
+        }
+        return series;
+    }
+}
 const MovementsHistory = ({data, categories}) => {
     const [showAssets, setShowAssets] = useState(localStorage.getItem("MovementsHistory.showAssets")??false);
     const balanceCategoryId = categories.find((cat) => cat.category === "BALANCE")?.id ?? -1;
     const isBalanceMovement = (movement) => movement.category === balanceCategoryId;
     const {i18n} = useLingui();
-    const baselineVal = data?.baseline?.at(1) ?? 0.;
-    let chartData = [];
-    let balance = 0;
-    if(data?.filtered?.movements) {
-        const _movements = data.filtered.movements;
-        // select BALANCE movements recorder BEFORE slice.minDate
-        const balance_movements = _movements.filter((movement) => {
-            let ret = isBalanceMovement(movement);
-            // if(slice){
-            //     const mdate = new Date(movement.date);
-            //     ret &= mdate <= slice.minDate;
-            // }
-            return ret;
-        });
-        if(balance_movements.length > 0){ 
-            // take the latest BALANCE record, which is a group of N>=1 movements and SUM their values. This will be the baseline
-            const latestDate = new Date(balance_movements[balance_movements.length-1].date);
-            balance_movements.filter((movement) => {
-                const mdate = new Date(movement.date);
-                return mdate.getTime() === latestDate.getTime();
-            }).forEach(movement => {
-                balance += movement.abs_amount;
-            });
-        }
-        let cumulative = balance>0? balance : baselineVal; //baseline value
+    let balance_to_date = data.previous.balance ?? { date: new Date(), value: 0.0 };
+    let cumulative = balance_to_date.value; /* baseline */
+    const non_balance_movements = [];
+    const balance_movements = new BalanceManager();
+    balance_movements.addMovement({"date": data.filtered.minDate, "abs_amount": balance_to_date.value})
 
-        for(let index=0; index<_movements.length; index++){
-            const movement = _movements.at(index);
-            const mDate = new Date(movement.date);
-            // if(slice && mDate.getTime()<slice.minDate){
-            //     continue;
-            // }
-            if(isBalanceMovement(movement)){ // this means category.direction == 0, i.e. this is a 'BALANCE' type movement
-                if(index>0){
-                // if(index>0 && (!slice || slice.minDate<=mDate)){
-                    // BALANCE movements come in blocks of N>=1 entries with the same exact date. We need to sort-of group them into one chart point
-                    let inner_movement = movement;
-                    let inner_balance = 0;
-                    while(isBalanceMovement(inner_movement)){
-                        const cur_date = new Date(inner_movement.date);
-                        inner_balance += inner_movement.abs_amount;
-                        index++;
-                        if(index>=_movements.length) break;
-                        inner_movement = _movements[index];
-                        const next_date = new Date(inner_movement.date);
-                        // the dates of balance items belonging to the same insertion can differ by some ~100ms
-                        // let's check the delta and consider two items be of the same insertion if their date diff is less than
-                        // a set value
-                        const next_ts = next_date.getTime();
-                        const cur_ts = cur_date.getTime();
-                        const diff = Math.abs(next_ts-cur_ts);
-                        if(diff > 2000){ // next mov could either be a balance or not, as long as the date differs we exit the loop
-                            break;
-                        }
-                    }
-                    balance = inner_balance;
-                }
+    if(data?.filtered.movements) {
+        const _movements = data.filtered.movements;
+
+        for (const movement of _movements) {
+            let ret = isBalanceMovement(movement);
+            if(ret){
+                balance_movements.addMovement(movement);
             }else{
+                const mDate = new Date(movement.date);
                 cumulative += movement.amount
+                non_balance_movements.push({"date": (mDate).getTime(), "cumulative": cumulative});
             }
-            // if(slice){
-            //     if(mDate <= slice.maxDate){
-            //         chartData.push({"date": (mDate).getTime(), "cumulative": cumulative, "balance": balance}) 
-            //     }
-            // }else{
-            //     chartData.push({"date": (mDate).getTime(), "cumulative": cumulative, "balance": balance})  
-            // }
-            chartData.push({"date": (mDate).getTime(), "cumulative": cumulative, "balance": balance})  
         }
+        
     }
     return (
         <div className='movements-history-container mt-2'>
@@ -91,16 +64,21 @@ const MovementsHistory = ({data, categories}) => {
                 </Form>
             </div>
             <ResponsiveContainer width="100%" height={400}>
-                <LineChart data={chartData}>
-                    <Line type="stepAfter" dataKey="cumulative" className='history-movements-chartline' dot={false} />
+                <LineChart>
+                    <Line type="bump" dataKey="cumulative" data={non_balance_movements} className='history-movements-chartline' dot={false} />
                     { showAssets?
-                        <Line type="stepAfter" dataKey="balance" className='history-assets-chartline' dot={false} />
+                        <Line type="stepAfter" dataKey="balance" 
+                            data={balance_movements.timeSeries()} 
+                            className='history-assets-chartline' 
+                            dot={true} strokeDasharray="5 5">
+                                <LabelList formatter={(value)=>parseFloat(value).toFixed(0)} position="insideBottomLeft" />
+                            </Line>
                         : null
                     }
                     <XAxis 
                         type='number' 
                         dataKey="date"  
-                        domain={[data?.minDate, data?.maxDate]} 
+                        domain={[(new Date(data.filtered.minDate)).getTime(), data?.filtered.maxDate]} 
                         // domain={["auto", "auto"]} 
                         tickFormatter={tick => (format(new Date(tick), i18n))}
                         tickCount="10" />
