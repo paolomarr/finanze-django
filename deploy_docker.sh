f0df87af0f93fc6df0f6b3cc3b7a5547cbfe1aaf
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2025-10-19 20:15:09 +0200

    Removing React Dockerfile and frontend deploy from deploy script

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -40,7 +40,6 @@ function generate_secret_key() {
 
 SCRIPT_ROOT=`cd $(dirname $0); pwd`
 MAIN_ENV_FILE=${SCRIPT_ROOT}/app/finanze/.env
-FE_ENV_FILE=${SCRIPT_ROOT}/movimenti-fe/.env
 PROFILE=${PROFILE:-production}
 
 while [[ $# -gt 0 ]]; do
@@ -51,17 +50,6 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
-base_version=`grep "\"version\":" movimenti-fe/package.json| head -n 1 | sed -E 's/[^0-9\.]//g'`
-commit=$(git rev-parse --short HEAD || echo "nvc")
-date=$(date '+%Y%m%d%H%M%S')
-version_info="$commit-$date"
-if [[ -n "$base_version" ]]; then
-    version_info=${base_version}-${version_info}
-fi
-
-# these exported will filter through the docker compose file
-export REACT_FRONTEND_TAG=finanze-django-react_frontend:${version_info}
-export VERSION_INFO=${version_info}
 
 if [[ ! -f $MAIN_ENV_FILE ]]; then
     info "Generating .env file for DJANGO runtime environment variables" >&2
@@ -85,27 +73,5 @@ else
         fi
     done
 fi
-if [[ ! -f $FE_ENV_FILE ]]; then
-    info "Generating .env file for REACT runtime environment variables" >&2
-    echo 'REACT_APP_BACKEND_API_BASE="http://localhost:8003"' >> $FE_ENV_FILE
-    echo "REACT_APP_VERSION_INFO=\"${VERSION_INFO}\"" >> $FE_ENV_FILE
-else
-    info "$FE_ENV_FILE file found"
-    found=`grep REACT_APP_BACKEND_API_BASE ${FE_ENV_FILE}`
-    if [[ -z $found ]]; then
-        error "The .env file has no REACT_APP_BACKEND_API_BASE variable defined. This is required, please edit the $FE_ENV_FILE file and add it."
-        exit 1
-    fi
-    found=`grep REACT_APP_VERSION_INFO ${FE_ENV_FILE} | cut -d "=" -f 2`
-    if [[ -z $found ]]; then
-        info "The .env file has no REACT_APP_VERSION_INFO. Adding it at the end."
-        echo "" >> ${FE_ENV_FILE}
-        echo "REACT_APP_VERSION_INFO=\"${VERSION_INFO}\"" >> $FE_ENV_FILE
-    else
-        info "Updating REACT_APP_VERSION_INFO to ${VERSION_INFO} (was $found)"
-        sed -i 's/REACT_APP_VERSION_INFO=.*$/REACT_APP_VERSION_INFO='${VERSION_INFO}'/' $FE_ENV_FILE
-    fi
-fi
-
 
 docker compose --profile $PROFILE up -d --build
